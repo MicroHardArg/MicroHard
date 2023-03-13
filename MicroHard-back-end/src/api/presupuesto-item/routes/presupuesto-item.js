e882898dc8a6546b8c7dbcd3a7c9c590c1ecdae6
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-03-12 23:33:21 -0400

    Added additional models and updated properties

---
+++ MicroHard-back-end/src/api/presupuesto-item/routes/presupuesto-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * presupuesto-item router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::presupuesto-item.presupuesto-item');
---
+++ MicroHard-back-end/src/api/presupuesto-item/services/presupuesto-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * presupuesto-item service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::presupuesto-item.presupuesto-item');
