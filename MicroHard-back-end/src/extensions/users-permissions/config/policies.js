9bcb2dca14088685737078af915a4004885ec3e7
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-03-23 02:41:18 -0400

    Attempt to use Auth0 for authentication

---
+++ MicroHard-back-end/src/extensions/users-permissions/config/policies.js
@@ -0,0 +1,9 @@
+module.exports = {
+    async isAuthenticated(ctx, next) {
+      if (!ctx.state.user) {
+        return ctx.unauthorized("You must be logged in to perform this action.");
+      }
+  
+      await next();
+    },
+  };  
