93149723f5d58e3569755da088f5f4cc9b5af595
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-03-28 12:36:29 -0400

    Fixed missing key prop error

--- client/components/clientes/filtroClientes.jsx
+++ client/components/clientes/filtroClientes.jsx
@@ -31,7 +31,7 @@ function Table({ data, searchTerm }) {
             </thead>
             <tbody>
             {filteredData.map((item) => (
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <tr key={item.id} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
                     <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>                        <div class="pl-3">
                             <div class="text-base font-semibold">{item.name}</div>
