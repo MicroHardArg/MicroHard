61c21797d172d6108e8f08f40631b2e4cc1a6805
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-04-12 13:18:00 -0400

    Modified Presupuesto form

--- client/src/pages/budgets.js
+++ client/src/pages/budgets.js
@@ -1,78 +1,196 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+import React, {useState, useEffect} from 'react'
+import Link from 'next/link'
 
-export default function BudgetsCreate() {
-  const [cantidad, setCantidad] = useState(1)
-  const [input, setInput] = useState({
-    data:{
-      cliente:"",
-      item:"",
-      descripcion:"",
-      precio:"",
-      IVA:"",
-      total:""
-    }   
-  });
-  const [inputsArray, setInputsArray] = useState([input]);
-
-  const [clients, setClients]= useState([]);
-
-  useEffect(() => {
-    async function fetchClients() {
-      const response = await fetch('http://localhost:1337/api/clientes');
-      const json = await response.json();
-      const data= json.data;
-      console.log("DATA",data);
-      setClients(data);
-    }
-    fetchClients();
-  }, []);
-
-  function handleChange(e, index) {
-    const { name, value } = e.target;
-    const newInputsArray = [...inputsArray];
-    newInputsArray[index] = {
-      data: {
-        ...inputsArray[index].data,
-        [name]: value
-      }
-    };
-    setInputsArray(newInputsArray);
-  }
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(inputsArray);
-    alert('Presupuesto Creado!!!');
-    setInputsArray([{ data: { cliente:"", descripcion:"", precio:"", IVA:"", total:"" } }]);
+export default function BudgetsCreate(){
     
-    try {
-        const response = await fetch('https://star-link-back-end-production.up.railway.app/', {
+    const [input, setInput] = useState({
+      data:{
+        cliente:"",
+        descripcion:"",
+        precio:"",
+        iva:"",
+        total:""
+           }
+     })
+
+    const [selectedItems, setSelectedItems]= useState({
+       id:"",
+       item:"",
+       cantidad:""
+     });
+
+    const [finalItems, setFinalItems]= useState([]);
+
+    const [items, setItems]= useState([]);
+
+    const [clients, setClients]= useState([]);
+
+    useEffect(() => {
+      var { precio, iva, total} = input.data;
+      if (!precio) {
+        iva="";
+        total="";
+      }
+      if (precio) {
+        precio=precio.toString().replace("," , ".");
+        iva=(parseFloat(precio)*0.21).toFixed(2);
+        let prevTotal = (parseFloat(precio)+parseFloat(iva)).toFixed(2);
+        total = parseFloat(prevTotal.toString().replace(",", ".")).toFixed(2);
+        
+      }
+      setInput((input) => ({
+        ...input,
+        data: {
+          ...input.data,
+          precio,
+          iva,
+          total
+        },
+      }));
+    }, [input.data.precio]);
+
+    useEffect(() => {
+      async function fetchClients() {
+        const response = await fetch('http://localhost:1337/api/clientes');
+        const json = await response.json();
+        const data= json.data;
+        setClients(data);
+      }
+      fetchClients();
+    }, []);
+
+    useEffect(() => {
+      async function fetchItems() {
+        const response = await fetch('http://localhost:1337/api/items');
+        const json = await response.json();
+        const data= json.data;
+        setItems(data);
+      }
+      fetchItems();
+    }, []);
+
+    function handleChange(e) {
+      setInput({
+        ...input,
+        data: {
+          ...input.data,
+          [e.target.name]: e.target.value
+        }
+      });
+    }
+
+    function handleItemName(e) {
+      var itemId = e.target.value;
+      const selectedItem = items.find((item) => item.id == itemId);
+      setSelectedItems({
+        ...selectedItems,
+        id: selectedItem.id,
+        item: selectedItem.attributes.nombre,
+      })
+    }
+
+    function handleItemAmount(e) {
+      var amount = e.target.value;
+      setSelectedItems({
+        ...selectedItems,
+        cantidad: amount
+      })
+    }
+    
+    function addItem() {
+      finalItems.push(selectedItems);
+      setSelectedItems({
+        id:"",
+        item:"",
+        cantidad:""
+      })
+      document.getElementsByName("item")[0].value = document.getElementsByName("item")[0].options[0].value;
+    };
+
+    function handleRemoveItem(id) {
+      const list = finalItems.filter((item) => item.id !== id);
+      setFinalItems(list);
+    };
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      console.log(input)
+      setInput({
+        data:{
+          cliente:"",
+          descripcion:"",
+          precio:"",
+          iva:"",
+          total:""
+           }
+     })
+      setSelectedItems({
+        id:"",
+        item:"",
+        cantidad:""
+      })
+      setFinalItems([]);
+
+     try {
+        let body={
+          data:{
+            cliente: input.data.cliente,
+            descripcion: input.data.descripcion,
+            precio: input.data.precio,
+            iva: input.data.iva,
+            total: input.data.total,
+            items: finalItems
+               }
+        }
+        console.log("BODY", body);
+        const response = await fetch('http://localhost:1337/api/presupuestos/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(inputsArray)
+          body: JSON.stringify(body)
         });
+        if (!response.ok) {
+          alert("No se pudo crear el Presupuesto");
+          throw new Error('Network response was not ok');
+        }
+        alert("Presupuesto creado satisfactoriamente");
         const data = await response.json();
         console.log(data);
+        
       } catch (error) {
         console.error(error);
       }
-  }
+    };
 
-  function handleAddRow() {
-    setInputsArray([...inputsArray, { data: { cliente:"", descripcion:"", precio:"", IVA:"", total:"" } }]);
-  }
+    
+
 
   return (
-                     <div>
-    <div className='mb-3'>
-                     <label className='mb-3 block text-base font-medium  text-black-200'>Cliente:</label>
+    <div>
+
+<div  className='flex w-full  h-full bottom-0 ' >
+
+<div className=' pb-0 '>
+            <img src="clientes.jpg" alt="backgroud" className='h-full w-full bg-cover' />
+            </div>
+
+            <div className='lg:bg-diagonal-section bg-home-bg overflow-hidden bg-no-repeat bg-center bg-home-responsive  w-full  mt-6 mb-20 absolute'>
+
+            <div className="mx-auto  bg-opacity-75 text-center rounded-lg max-w-screen-xl px-4 py-10 lg:items-center pb-32">
+
+              
+
+<h1  className='text-xl font-extrabold sm:text-5xl text-white'>Crear Nuevo Presupuesto</h1>
+
+<form className='bg-zinc-800  p-5 mt-10 rounded-xl mx-auto w-full max-w-[550px] border-white border-0 shadow-sm shadow-white' onSubmit={(e)=>handleSubmit(e)}>
+
+                <div className='mb-3'>
+                     <label className='mb-3 block text-base font-medium  text-gray-200'>Cliente:</label>
                    <select 
-                   className="w-full rounded-md border border-[#fcfcfc] bg-transparent py-3 px-6 text-base font-medium text-[gray] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                   className="w-full rounded-md border border-[#fcfcfc] bg-transparent py-3 px-6 text-base font-medium text-[#ffffff] outline-none focus:border-[#6A64F1] focus:shadow-md"
                     type="text" 
-                    value= {input.cliente}
+                    value= {input.data.cliente}
                     name= "cliente"
                     onChange={(e)=> handleChange(e)}
                     >
@@ -84,67 +202,118 @@ export default function BudgetsCreate() {
                       ))}
                   </select>  
                 </div>
-      {inputsArray.map((input, index) => (
-        <div key={index}>
-          <input
-            name="item"
-            value={input.data.item}
-            onChange={(e) => handleChange(e, index)}
-            placeholder="Item"
-          />
+                
+              <div className='mb-3'>
+                     <label className='mb-3 block text-base font-medium  text-gray-200'>Descripción:</label>
+                   <input 
+                   className="w-full rounded-md border border-[#fcfcfc] bg-transparent py-3 px-6 text-base font-medium text-[#ffffff] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    type="text" 
+                     value= {input.data.descripcion}
+                     name= "descripcion"
+                 onChange={(e)=> handleChange(e)}
+                    />
+                    
+                </div>
+
+                <div className='mb-3'>
+                     <label className='mb-3 block text-base font-medium  text-gray-200'>Items:</label>
+                   <select 
+                   className="w-full rounded-md border border-[#fcfcfc] bg-transparent py-3 px-6 text-base font-medium text-[#ffffff] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    type="text" 
+                    name= "item"
+                    onChange={(e)=> handleItemName(e)}
+                    >
+                      <option value="">Selecciona un Item</option>
+                      {items.map((item) => (
+                        <option key={item.id} value={item.id}>
+                        {item.attributes.nombre}
+                        </option>
+                      ))}
+                  </select>  
+                  <input
+                  id='amount'
+                  className="w-full rounded-md border  border-[#fcfcfc] bg-transparent py-3 px-6 text-base font-medium text-[#ffffff] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  value={selectedItems.cantidad}
+                  name='cantidad'
+                  placeholder='Cantidad'
+                  onChange={(e)=> handleItemAmount(e)}
+                  />
+                  <button
+                  type='button'
+                  onClick={(e)=> addItem(e)}>
+                    Agregar
+                  </button>
+
+                  <ul>
+                    {finalItems.map((item) => (
+                      <li key={item.id}>
+                        {item.item}, {item.cantidad}
+                        <button 
+                          onClick={() => handleRemoveItem(item.id)}>X
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+                <div className='mb-3'>
+                     <label className='mb-3 block text-base font-medium  text-gray-200'>Precio:</label>
+                   <input 
+                   className="w-full rounded-md border border-[#fcfcfc] bg-transparent py-3 px-6 text-base font-medium text-[#ffffff] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                     value= {input.data.precio}
+                     name= "precio"
+                     onChange={(e)=> handleChange(e)}
+                    />
+                    
+                </div>
+
+                <div className='mb-3'>
+                     <label className='mb-3 block text-base font-medium  text-gray-200'>IVA:</label>
+                   <input 
+                   className="w-full rounded-md border border-[#fcfcfc] bg-transparent py-3 px-6 text-base font-medium text-[#ffffff] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                     value= {input.data.iva}
+                     name= "iva"
+                     readOnly
+                     onChange={(e)=> handleChange(e)}
+                    />
+                    
+                </div>
+
+                <div className='mb-3'>
+                     <label className='mb-3 block text-base font-medium  text-gray-200'>Total:</label>
+                   <input 
+                   className="w-full rounded-md border border-[#fcfcfc] bg-transparent py-3 px-6 text-base font-medium text-[#ffffff] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                     value= {input.data.total}
+                     name= "total"
+                     onChange={(e)=> handleChange(e)}
+                    />
+                    
+                </div>
+
+               
+        <div className=' pt-3 pb-3'>   
+      <div className="text-4xl bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline "
+        type="submit" >
+      <button type="submit">Crear Presupuesto</button>
+      </div>
+      </div> 
+    </form>
+
+    <div className='pt-8'> 
         
-            <select>
-            name="cantidad"
-            value={input.data.cantidad}
-            onChange={(e) => handleChange(e, index)}
-            placeholder="Cantidad"
-            <option value="">Cantidad</option>
-            <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="8">9</option>
-                <option value="10">10</option>
-            </select>
-          
-
-
-          
-          <input
-            name="descripcion"
-            value={input.data.descripcion}
-            onChange={(e) => handleChange(e, index)}
-            placeholder="Descripción"
-          />
-          <input
-            name="precio"
-            value={input.data.precio}
-            onChange={(e) => handleChange(e, index)}
-            placeholder="Precio"
-          />
-          <input
-            name="IVA"
-            value={input.data.IVA}
-            onChange={(e) => handleChange(e, index)}
-            placeholder="IVA"
-          />
-          <input
-            name="total"
-            value={input.data.total}
-            onChange={(e) => handleChange(e, index)}
-            placeholder="Total"
-          />
-        </div>
-      ))}
-      <button onClick={handleAddRow}>Agregar fila</button>
-      <button onClick={handleSubmit}>Guardar</button>
-      <Link href="/budgets">
-        <button>Cancelar</button>
+    <Link href= '/'>
+      <button className="text-gray-50  bg-blue-900 px-10 py-1 hover:bg-blue-700 rounded-full">Return</button>
+      
       </Link>
+</div>
+
     </div>
-  );
+
+    </div>
+
+    </div>
+
+    </div>
+    
+  )
 }
