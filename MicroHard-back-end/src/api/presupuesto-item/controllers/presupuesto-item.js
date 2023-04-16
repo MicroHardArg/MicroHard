7f6420c9d5d844a416cdaf83d5b8fefc7194b713
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-04-16 14:34:29 -0400

    Modified Presupuesto model

--- MicroHard-back-end/src/api/presupuesto-item/controllers/presupuesto-item.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * presupuesto-item controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::presupuesto-item.presupuesto-item');
