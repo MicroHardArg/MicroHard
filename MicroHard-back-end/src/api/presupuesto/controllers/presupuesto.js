f32b2aa357c312b8ab95c5a8537adcc923530312
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-05-08 02:41:38 -0400

    Solved budget controller error

--- MicroHard-back-end/src/api/presupuesto/controllers/presupuesto.js
+++ MicroHard-back-end/src/api/presupuesto/controllers/presupuesto.js
@@ -6,55 +6,7 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::presupuesto.presupuesto',{
-    async create(ctx) {
-        const {cliente, descripcion, total}= ctx.request.body.data;
-
-        // Gets previous entries from the client
-        try {
-            var entries=await strapi.db.query('api::presupuesto.presupuesto').findMany({
-                where: {cliente: cliente},
-                orderBy: { id: 'ASC' },
-            });
-    
-        // Calculates total debt from previous entries
-            // var totales=0;
-    
-            // entries.map(entry => {
-            //     if (entry.tipo==="Presupuesto" || entry.tipo==="Recurrente") {
-            //         total=total+entry.monto;
-            //     }
-            //     if (entry.tipo==="Abono") {
-            //         total=total-entry.monto;
-            //     }
-            // });
-
-        // Calculates final debt based on recent amount
-            // if (tipo==="Presupuesto" || tipo==="Recurrente") {
-            //     total=total+monto;
-            // }
-            // else if (tipo==="Abono") {
-            //     total=total-monto;
-            // };
-    
-        // Assigns debt to property "deuda"
-            // ctx.request.body.data.deuda=total;
-    
-        // Creates new entry for the client
-            return strapi.db.query('api::presupuesto.presupuesto').create({
-                data: {
-                    cliente: cliente,
-                    descripcion: descripcion,
-                    total: total,
-                }
-            });
-
-        }
-        catch(error) {
-            console.log(error);
-        };
-    }
-});;
+module.exports = createCoreController('api::presupuesto.presupuesto');
 
 
 
